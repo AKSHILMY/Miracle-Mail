74850585ec233fefb38c22744341fff1738b3c2b
Akeel Ahmed <77114086+AKSHILMY@users.noreply.github.com>
2021-10-12 09:05:03 +0530

    Update tkinterGUI.py

--- tkinterGUI.py
+++ tkinterGUI.py
@@ -3,12 +3,32 @@
 # Give access to third part apps in gmail security settings
 # Install smtp library using 'pip install secure-smtplib' command in command prompt 
 import tkinter
-from tkinter import filedialog
+from tkinter import filedialog,ttk
 from tkinter.constants import DISABLED, E, NW, W, X
-
-
+from email.mime.multipart import MIMEMultipart
 import smtplib,socket
 
+# the message to be sent using email
+msg = MIMEMultipart()       # create a message
+
+
+# main window creation
+height,width  = 640,420
+window = tkinter.Tk()
+window.geometry(str(height)+'x'+str(width))
+window.resizable(0,0)
+window.title("Miracle Mail")
+
+# strings to be used throught the program
+recipient_email_str = tkinter.StringVar()
+sender_email_str = tkinter.StringVar()
+password_str = tkinter.StringVar()
+
+# list of attachments     
+attachments_list=[]
+
+
+# setting up server, authentication , creating email,attachment addition, sending email
 def setup_server():
     # set up the SMTP server
     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -16,18 +36,16 @@ def setup_server():
     return server
 
 def login(server):
-    sender_address = str(sender.get())
-    sender_password = str(password_str.get())
-    server.login(sender_address,sender_password)
-    return sender_address
+    email_address = str(sender_email_str.get())
+    email_password = str(password_str.get())
+    server.login(email_address,email_password)
+    return email_address
 
 def create_email(address):
     global msg
-    from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
     
-    recipient_email = str(email.get())
-
+    recipient_email = str(recipient_email_str.get())
     # setup the parameters of the message
     msg['From'] = address
     msg['To'] = recipient_email
@@ -43,13 +61,8 @@ def send(server,msg):
     # send the message via the server set up earlier.
     server.send_message(msg)
     del msg
-    tkinter.messagebox.showinfo(title="Successful", message="Email: "+str(str(email.get())+"\n"+"Subject: "+subject_text.get("1.0",'end-1c'))+"\n"+"Message: "+str(message_text.get("1.0",'end-1c')))
-    print(str(subject_text.get("1.0",'end-1c')))
-    print(str(message_text.get("1.0",'end-1c')))
-    print(str(email.get()))
     print("Successful")
     
-
 def send_email():
     from tkinter import messagebox
     try:
@@ -57,6 +70,11 @@ def send_email():
         address  = login(server)
         msg = create_email(address)
         send(server,msg)
+        add_attachments(attachments_list)
+        
+        print(str(subject_text.get("1.0",'end-1c')))
+        print(str(message_text.get("1.0",'end-1c')))
+        print(str(recipient_email_str.get()))
     except socket.gaierror:
         error = "No Internet Connection"
         tkinter.messagebox.showerror(title="Error", message=error)
@@ -77,45 +95,21 @@ def send_email():
         tkinter.messagebox.showerror(title="Error", message=error)
         print(error)
         
-
-def send_window_process():
-    print(str(subject_text.get("1.0",'end-1c')))
-    print(str(message_text.get("1.0",'end-1c')))
-    print(str(email.get()))
-    print()
-    print("Top Level")
-    loginWindow = tkinter.Toplevel(master=canvas)
-    loginWindow.grab_set() # to prevent access to main window
-    loginWindow.title("Login")
-    loginWindow.geometry("440x100")
-    loginWindow.config(background="#000000")
-    loginWindow.resizable(0,0)
-    img2=tkinter.PhotoImage(file="D:\Github\Simple-Projects\miraclemaillogo.png")
-    loginWindow.iconphoto(False,img2)
+def show_attachments(): 
+    from tkinter import messagebox
+    global attachments_list
+    filenames=""
+    if len(attachments_list)>0:
+        filenames = "Following files were attached to the email: "+"\n\n"+"\n".join(attachments_list)
+    else:
+        filenames = "No attachments attached"
+        
+    tkinter.messagebox.showinfo(title = "Attachments",message=filenames)
     
-    sender_email_address = tkinter.Label(loginWindow,text="Email of Sender: ",bg='#000080',fg="#FFFFFF")
-    sender_email_address.grid(row=1, column=0, sticky=W, pady=10,  ipadx='5px',padx=10)
-    sender_email_address_entry = tkinter.Entry(loginWindow,width=40,textvariable=sender)
-    sender_email_address_entry.grid(row=1,column=1,sticky=W,padx=10)
-
-    password = tkinter.Label(loginWindow,text="Password",bg='#000080',fg="#FFFFFF")
-    password.grid(row=2, column=0,sticky=W, pady=5,  ipadx='5px',padx=10) 
-    password_text = tkinter.Entry(loginWindow,width=25,textvariable=password_str,show='*')
-    password_text.grid(row=2,column=1,sticky=W,padx=10)
-
-   
-    
-    send = tkinter.Button(loginWindow,text="Send",bg='#000080',fg="#FFFFFF",command=send_email)
-    send.grid(row=3,column=2,sticky=W)
-
-
-def add_attachment(): 
-    from email.mime.base import MIMEBase
-    from email import encoders
-
+def choose_attachments():
     from tkinter import filedialog
 
-    
+
     filetypes = (
         ('All files', '*.*'),
         ('text files', '*.txt'),
@@ -126,88 +120,146 @@ def add_attachment():
         title='Open a file',
         initialdir='/',
         filetypes=filetypes)
-
-    # open the file to be sent 
     if filename:
-        file = open(filename, "rb")
-        # instance of MIMEBase and named as attachment
-        attachment = MIMEBase('application', 'octet-stream')
-        
-        # To change the payload into encoded form
-        attachment.set_payload((file).read())
-        
-        # encode into base64
-        encoders.encode_base64(attachment)
-        # attachment.add_header(input("Add header to the attachment: ").strip(),"client")
-        
-        # attach the instance 'attachment' to instance 'msg'
-        msg.attach(attachment)
-        print("Attachment has been added")
+        attachments_list.append(filename)
+
+def add_attachments(attachments): 
+    from email.mime.base import MIMEBase
+    from email import encoders
+    import os
+
+    # new window to show that attachments are uploading 
+    uploadWindow = tkinter.Toplevel(canvas)
+    uploadWindow.grab_set() # to prevent access to main window
+    uploadWindow.title("Uploading")
+    uploadWindow.geometry("240x50")
+    uploadWindow.config(background="#000000")
+    uploadWindow.resizable(0,0)
+
+    label = tkinter.Label(uploadWindow, text="00%")
+    label.pack()
+
+    progressbar = tkinter.ttk.Progressbar(uploadWindow, orient="horizontal", length=100,  mode='determinate')
+    progressbar.pack()
+    for filename in attachments:
+        # open the file to be sent 
+        if filename:
+            file = open(filename, "rb")
+            # instance of MIMEBase and named as attachment
+            attachment = MIMEBase('application', 'octet-stream')
+            
+            # To change the payload into encoded form
+            attachment.set_payload((file).read())
+            
+            # encode into base64
+            encoders.encode_base64(attachment)
+            # attachment.add_header(input("Add header to the attachment: ").strip(),"client")
+            
+            # attach the instance 'attachment' to instance 'msg'
+            msg.attach(attachment)
+            filesize = os.path.getsize(filename)
+
+            # progress bar for uploading
+            with open(filename, "r", encoding='utf-8') as f:
+                for i in range(os.path.getsize(filename)):
+                    progressbar["value"] = progressbar["value"]+1
+                    progressbar["maximum"] = filesize
+                    label.config(text="Uploading "+filename+str(int(progressbar["value"]/os.path.getsize(filename)*100)) + "%")          
+                    canvas.update_idletasks()
+                    #time.sleep(1) 
+                progressbar["value"]=0
+
+            print("Attachment has been added")
+            if filename==attachments[-1]:
+                uploadWindow.destroy()
+   
+
+def send_window_process():
+    print(str(subject_text.get("1.0",'end-1c')))
+    print(str(message_text.get("1.0",'end-1c')))
+    print(str(recipient_email_str.get()))
+    print()
+    print("Top Level")
+
+    # new window for login process 
+    loginWindow = tkinter.Toplevel(master=canvas)
+    loginWindow.grab_set() # to prevent access to main window
+    loginWindow.title("Login")
+    loginWindow.geometry("440x100")
+    loginWindow.config(background="#000000")
+    loginWindow.resizable(0,0)
+    # img2=tkinter.PhotoImage(file="D:\Github\Simple-Projects\miraclemaillogo.png")
+    # loginWindow.iconphoto(False,img2)
+    
+    sender_email_address = tkinter.Label(loginWindow,text="Email of Sender: ",bg='#000080',fg="#FFFFFF")
+    sender_email_address.grid(row=1, column=0, sticky=W, pady=10,  ipadx='5px',padx=10)
+    sender_email_address_entry = tkinter.Entry(loginWindow,width=40,textvariable=sender_email_str)
+    sender_email_address_entry.grid(row=1,column=1,sticky=W,padx=10)
+
+    password = tkinter.Label(loginWindow,text="Password",bg='#000080',fg="#FFFFFF")
+    password.grid(row=2, column=0,sticky=W, pady=5,  ipadx='5px',padx=10) 
+    password_text = tkinter.Entry(loginWindow,width=25,textvariable=password_str,show='*')
+    password_text.grid(row=2,column=1,sticky=W,padx=10)
+
+    send = tkinter.Button(loginWindow,text="Send",bg='#000080',fg="#FFFFFF",command=send_email)
+    send.grid(row=3,column=2,sticky=W)
+
+
+
     
 # GUI
-from email.mime.multipart import MIMEMultipart
-msg = MIMEMultipart()       # create a message
+if __name__ == "__main__":
+
+   
+    icon = tkinter.PhotoImage(file="D:/guiImage.png")
+
+    canvas = tkinter.Canvas(window,width=width,height=height,background="#000000")
+    canvas.pack(fill='both',expand=True)
+    # canvas.create_image(0,0,image=icon,anchor=NW)
+
+    img1=tkinter.PhotoImage(master=window,file="D:\Github\Simple-Projects\miraclemaillogo.png")
+    window.iconphoto(False,img1)
+
+    img2=tkinter.PhotoImage(master=canvas,file="D:\Github\Simple-Projects\miraclemaillogo.png")
+
+    logo_label = tkinter.Label(canvas,image=img2)
+    logo_label.grid(row=0,column=0,sticky=W,pady=10,padx=60)
 
 
+    brand_label = tkinter.Label(canvas, text="MIRACLE MAIL", bg="#171717",fg='#ffffff', font='arial 20 bold')
+    brand_label.grid(row=0, column=1, sticky=W, pady=2, padx='15px')
 
-height = 640
-width = 360
+    recipient_email_label = tkinter.Label(canvas,text="Email of Recipient: ",bg='#000080',fg="#FFFFFF")
+    subject_label = tkinter.Label(canvas,text="Subject",bg='#000080',fg="#FFFFFF")
+    msg_label = tkinter.Label(canvas,text="Message",bg='#000080',fg="#FFFFFF")
+
+    recipient_email_label.grid(row=1, column=0, sticky=W, pady=5,  ipadx='5px',padx=10)
+    subject_label.grid(row=2, column=0,sticky=W, pady=5,  ipadx='5px',padx=10) 
+    msg_label.grid(row=3,column=0,sticky=W, pady=5,  ipadx='5px',padx=10)
+
+    email_address_entry = tkinter.Entry(canvas,width=30,textvariable=recipient_email_str)
+    email_address_entry.grid(row=1,column=1,sticky=W,padx=10)
+
+    # verify = tkinter.Button(canvas,text="Verify Email address",bg='#000080',fg="#FFFFFF",command=verify_email)
+    # verify.grid(row=1,column=3,sticky=W,padx=10)
+
+    subject_text = tkinter.Text(canvas,height=1,width=30)
+    subject_text.grid(row=2,column=1,sticky=W,padx=10)
+
+    message_text = tkinter.Text(canvas,height=10,width=44) 
+    message_text.grid(row=6,columnspan=2,sticky=W,padx=10,pady=5)
+
+    attachment_button = tkinter.Button(canvas,text="Attach File",bg='#C0C0C0',fg="#000000",command=choose_attachments)
+    attachment_button.grid(row=11,column=0,sticky=W,padx=10)
+
+    proceed_button = tkinter.Button(canvas,text="Proceed",bg='#C0C0C0',fg="#000000",command=send_window_process)
+    proceed_button.grid(row=11,column=1,sticky=E)
+
+    files_button = tkinter.Button(canvas,text="...",bg='#C0C0C0',fg="#000000",command=show_attachments,height=1,width=3)
+    files_button.grid(row=12,column=0,sticky=W,padx=10)
 
 
-window = tkinter.Tk()
-email = tkinter.StringVar()
-sender = tkinter.StringVar()
-password_str = tkinter.StringVar()
+    window.mainloop()
 
-window.geometry(str(height)+'x'+str(width))
-window.resizable(0,0)
-window.title("Miracle Mail")
-icon = tkinter.PhotoImage(file="D:/guiImage.png")
-
-canvas = tkinter.Canvas(window,width=width,height=height)
-canvas.pack(fill='both',expand=True)
-canvas.create_image(0,0,image=icon,anchor=NW)
-
-img1=tkinter.PhotoImage(master=window,file="D:\Github\Simple-Projects\miraclemaillogo.png")
-window.iconphoto(False,img1)
-
-img2=tkinter.PhotoImage(master=canvas,file="D:\Github\Simple-Projects\miraclemaillogo.png")
-
-logo = tkinter.Label(canvas,image=img2)
-logo.grid(row=0,column=0,sticky=W,pady=10,padx=60)
-
-
-brand = tkinter.Label(canvas, text="MIRACLE MAIL", bg="#171717",fg='#ffffff', font='arial 20 bold')
-brand.grid(row=0, column=1, sticky=W, pady=2, padx='15px')
-
-email_address = tkinter.Label(canvas,text="Email of Recipient: ",bg='#000080',fg="#FFFFFF")
-subject = tkinter.Label(canvas,text="Subject",bg='#000080',fg="#FFFFFF")
-message = tkinter.Label(canvas,text="Message",bg='#000080',fg="#FFFFFF")
-
-email_address.grid(row=1, column=0, sticky=W, pady=5,  ipadx='5px',padx=10)
-subject.grid(row=2, column=0,sticky=W, pady=5,  ipadx='5px',padx=10) 
-message.grid(row=3,column=0,sticky=W, pady=5,  ipadx='5px',padx=10)
-
-email_address_entry = tkinter.Entry(canvas,width=30,textvariable=email)
-email_address_entry.grid(row=1,column=1,sticky=W,padx=10)
-
-# verify = tkinter.Button(canvas,text="Verify Email address",bg='#000080',fg="#FFFFFF",command=verify_email)
-# verify.grid(row=1,column=3,sticky=W,padx=10)
-
-subject_text = tkinter.Text(canvas,height=1,width=30)
-subject_text.grid(row=2,column=1,sticky=W,padx=10)
-
-message_text = tkinter.Text(canvas,height=10,width=44) 
-message_text.grid(row=6,columnspan=2,sticky=W,padx=10,pady=5)
-
-attachment = tkinter.Button(canvas,text="Attach File",bg='#C0C0C0',fg="#000000",command=add_attachment)
-attachment.grid(row=11,column=0,sticky=W,padx=10)
-
-proceed = tkinter.Button(canvas,text="Proceed",bg='#C0C0C0',fg="#000000",command=send_window_process)
-proceed.grid(row=11,column=1,sticky=E)
-
-window.mainloop()
-
-# To be used later
 
 
